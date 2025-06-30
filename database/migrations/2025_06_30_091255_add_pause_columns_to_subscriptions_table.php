c041b82edd94a22dce4ec90f05b430c4f407ddd6
riksa paradila pasa <riksaparadila202@gmail.com>
2025-06-30 16:26:11 +0700

    add: kolom pause subscription

---
+++ database/migrations/2025_06_30_091255_add_pause_columns_to_subscriptions_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tbl_subscriptions', function (Blueprint $table) {
+            $table->date('pause_start')->nullable()->after('status');
+            $table->date('pause_end')->nullable()->after('pause_start');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            //
+        });
+    }
+};
